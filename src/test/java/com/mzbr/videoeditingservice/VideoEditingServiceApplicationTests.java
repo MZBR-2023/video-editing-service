610e7dd3935d41e98eebac0f8803eaf8a69ddbe8
최주연 <genboyjy@gmail.com>
2023-10-26 13:20:15 +0900

    :zap: feat: 자막 기능 추가

--- src/test/java/com/mzbr/videoeditingservice/VideoEditingServiceApplicationTests.java
+++ src/test/java/com/mzbr/videoeditingservice/VideoEditingServiceApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.mzbr.videoeditingservice.model.Clip;
 import com.mzbr.videoeditingservice.model.Crop;
+import com.mzbr.videoeditingservice.model.Subtitle;
 import com.mzbr.videoeditingservice.model.UserUploadAudioEntity;
 import com.mzbr.videoeditingservice.model.VideoEntity;
 import com.mzbr.videoeditingservice.service.VideoEditingService;
@@ -44,7 +45,29 @@ class VideoEditingServiceApplicationTests {
 			.height(1280)
 			.crop(Crop.builder().startX(100).startY(100).width(100).height(100).zoomFactor(2).build())
 			.build();
+		Subtitle subtitle1 = Subtitle.builder()
+			.color(16711680)
+			.text("테스트1")
+			.scale(2.0F)
+			.startTime(1000)
+			.endTime(5000)
+			.id(1L)
+			.positionX(100)
+			.positionY(100)
+			.zIndex(1)
+			.build();
 
+		Subtitle subtitle2	 = Subtitle.builder()
+			.color(65280 )
+			.text("good")
+			.scale(0.6F)
+			.startTime(3000)
+			.endTime(8000)
+			.id(1L)
+			.positionX(100)
+			.positionY(100)
+			.zIndex(3)
+			.build();
 		UserUploadAudioEntity userUploadAudioEntity = UserUploadAudioEntity.builder()
 			.id(1L)
 			.url("test.mp3")
@@ -54,7 +77,9 @@ class VideoEditingServiceApplicationTests {
 
 		VideoEntity videoEntity = VideoEntity.builder()
 			.clips(List.of(clip1,clip2))
+			.subtitles((List.of(subtitle1,subtitle2)))
 			.userUploadAudioEntity(userUploadAudioEntity)
+			.videoUuid("uuid")
 			.id(1L)
 			.build();
 		// videoEditingService.processVideo(videoEntity,"out.mp4");
