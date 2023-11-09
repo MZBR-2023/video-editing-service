916841a8a79a3330f150820c91a8027f7b712988
최주연 <genboyjy@gmail.com>
2023-11-09 09:56:19 +0900

    :zap: feat: 임시 비디오용 엔티티, 레포지터리 추가

---
+++ src/main/java/com/mzbr/videoeditingservice/model/TempCrop.java
@@ -0,0 +1,36 @@
+package com.mzbr.videoeditingservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "temp_crop")
+public class TempCrop {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+
+	Integer x;
+	Integer y;
+	Integer width;
+	Integer height;
+
+	@OneToOne
+	@JoinColumn(name = "temp_video_id")
+	TempVideo tempVideo;
+
+}
---
+++ src/main/java/com/mzbr/videoeditingservice/model/TempVideo.java
@@ -0,0 +1,41 @@
+package com.mzbr.videoeditingservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "temp_video")
+@ToString
+public class TempVideo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String videoName;
+	private String originVideoUrl;
+	private String afterCropUrl;
+
+	@OneToOne(mappedBy = "tempVideo")
+	TempCrop tempCrop;
+
+	public void updateAfterCropUrl(String afterCropUrl) {
+		this.afterCropUrl=afterCropUrl;
+	}
+
+	public void updateOriginCropUrl(String originVideoUrl) {
+		this.originVideoUrl=originVideoUrl;
+	}
+}
