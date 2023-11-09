27934de30decbcac2fdb767028c3424ab38ebb45
최주연 <genboyjy@gmail.com>
2023-11-09 09:55:53 +0900

    :zap: feat: S3유틸 기능 추가 & 수정
    로컬파일 업로드 기능 메소드 분리
    클라이언트 업로드용 프리사인 URL 발급 기능 추가

--- src/main/java/com/mzbr/videoeditingservice/util/S3Util.java
+++ src/main/java/com/mzbr/videoeditingservice/util/S3Util.java
@@ -1,5 +1,6 @@
 package com.mzbr.videoeditingservice.util;
 
+import java.net.URL;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.HashMap;
@@ -14,12 +15,14 @@ import com.mzbr.videoeditingservice.model.Clip;
 
 import lombok.RequiredArgsConstructor;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 
 @Component
 @RequiredArgsConstructor
@@ -32,13 +35,21 @@ public class S3Util {
 
 	public void uploadLocalFileByStringFormat(List<Path> pathList, String folderName) {
 		for (Path path : pathList) {
-			PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-				.bucket(BUCKET_NAME)
-				.key(folderName + "/" + path.getFileName().toString())
-				.build();
-
-			s3Client.putObject(putObjectRequest, RequestBody.fromFile(path.toFile()));
+			uploadLocalFile(path,folderName + "/" + path.getFileName().toString());
 		}
+	}
+	public String fileUrl(String fileName){
+		return "https://" + BUCKET_NAME + ".s3." + Region.AP_NORTHEAST_2.toString() + ".amazonaws.com/" + fileName;
+	}
+	public String uploadLocalFile(Path path,String uploadUrl){
+		PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+			.bucket(BUCKET_NAME)
+			.key(uploadUrl)
+			.build();
+
+		s3Client.putObject(putObjectRequest, RequestBody.fromFile(path.toFile()));
+
+		return uploadUrl;
 	}
 
 	public Map<Long, String> getClipsPresignedUrlMap(Set<Clip> clips) {
@@ -63,5 +74,20 @@ public class S3Util {
 
 		PresignedGetObjectRequest presignedGetObjectRequest = s3Presigner.presignGetObject(getObjectPresignRequest);
 		return presignedGetObjectRequest.url().toString();
+	}
+
+	public String generatePresignedUrl(String objectName) {
+		PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+			.bucket(BUCKET_NAME)
+			.key(objectName)
+			.build();
+
+		PresignedPutObjectRequest presignedPutObjectRequest =
+			s3Presigner.presignPutObject(z -> z.signatureDuration(Duration.ofMinutes(10)).putObjectRequest(putObjectRequest));
+
+		URL url = presignedPutObjectRequest.url();
+
+
+		return url.toString();
 	}
 }
