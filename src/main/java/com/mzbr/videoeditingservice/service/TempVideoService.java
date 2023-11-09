304d1c7944dd497943025da5a1f9b075f71cdb78
최주연 <genboyjy@gmail.com>
2023-11-09 09:59:09 +0900

    :zap: feat: 임시 비디오 서비스 추가

---
+++ src/main/java/com/mzbr/videoeditingservice/service/TempVideoService.java
@@ -0,0 +1,8 @@
+package com.mzbr.videoeditingservice.service;
+
+import com.mzbr.videoeditingservice.dto.UploadTempVideoDto;
+
+public interface TempVideoService {
+
+	String UploadTempVideo(UploadTempVideoDto uploadTempVideoDto);
+}
