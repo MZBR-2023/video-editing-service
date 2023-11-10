e8d14582902c5b87d86ac80923e20e317d675c1c
최주연 <genboyjy@gmail.com>
2023-11-10 17:09:30 +0900

    :zap: feat: Member 엔티티 추가

---
+++ src/main/java/com/mzbr/videoeditingservice/model/Member.java
@@ -0,0 +1,26 @@
+package com.mzbr.videoeditingservice.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "member")
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "memberId")
+	Integer id;
+}
