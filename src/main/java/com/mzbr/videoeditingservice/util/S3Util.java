cdf0350f04282ec4dea77fac8dc67a7c48333c7e
최주연 <genboyjy@gmail.com>
2023-11-10 14:57:36 +0900

    :zap: feat: s3 파일 다운로드 기능 추가

--- src/main/java/com/mzbr/videoeditingservice/util/S3Util.java
+++ src/main/java/com/mzbr/videoeditingservice/util/S3Util.java
@@ -1,5 +1,9 @@
 package com.mzbr.videoeditingservice.util;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Path;
 import java.time.Duration;
@@ -7,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -14,10 +19,12 @@ import org.springframework.stereotype.Component;
 import com.mzbr.videoeditingservice.model.Clip;
 
 import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
@@ -89,5 +96,21 @@ public class S3Util {
 
 
 		return url.toString();
+	}
+
+	public Path getFileToLocalDirectory(String fileName) throws IOException {
+		GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+			.bucket(BUCKET_NAME)
+			.key(fileName)
+			.build()
+			;
+		ResponseBytes<GetObjectResponse> objectBytes = s3Client.getObjectAsBytes(getObjectRequest);
+		byte[] data = objectBytes.asByteArray();
+
+		File myFile = new File(UUID.randomUUID().toString()+".mp4");
+		OutputStream os = new FileOutputStream(myFile);
+		os.write(data);
+		os.close();
+		return myFile.getAbsoluteFile().toPath();
 	}
 }
