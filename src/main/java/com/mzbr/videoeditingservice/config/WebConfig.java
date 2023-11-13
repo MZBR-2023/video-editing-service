a6550362cf68ece1976ce574014eff0009e2b551
최주연 <genboyjy@gmail.com>
2023-11-13 17:03:15 +0900

    :bug: fix: cors 문제 진짜 해결

--- src/main/java/com/mzbr/videoeditingservice/config/WebConfig.java
+++ src/main/java/com/mzbr/videoeditingservice/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.mzbr.videoeditingservice.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -25,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*")
+			.allowedOriginPatterns("*")
 			.allowedMethods("*")
 			.allowedHeaders("*")
 			.allowCredentials(true);
